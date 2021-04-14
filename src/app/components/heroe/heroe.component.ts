1cebc780be231d017fee2bd65d0383c3b745e264
Javier <jndominguez@isotrol.com>
2021-04-14 08:44:13 +0200

    63

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from 'src/app/servicios/heroes.service';
+
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styles: []
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
 
-  constructor() { }
+  heroe:any = {}; 
 
-  ngOnInit() {
-  }
+  constructor( private activatedRoute: ActivatedRoute,
+               private _heroesService: HeroesService) {
+
+    this.activatedRoute.params.subscribe( params => {
+      this.heroe = this._heroesService.getHeroe(params['id']);
+      console.log(this.heroe);
+    })
+   }
 
 }
